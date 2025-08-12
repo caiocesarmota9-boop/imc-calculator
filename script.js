7b1daf8dfd76b80ca961587f0d3dc8064e7dcda5
Caio Mota <caiocesarmota9@gmail.com>
2025-08-12 16:28:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function calcularIMC() {
     const imc = (peso / (altura ** 2)).toFixed(2);
     let classificacao = "";
 
-    if (imc < 18.5) classificacao = "Abaixo do peso";
-    else if (imc < 24.9) classificacao = "Peso normal";
-    else if (imc < 29.9) classificacao = "Sobrepeso";
+    if (imc < 18,5) classificacao = "Abaixo do peso";
+    else if (imc < 24,9) classificacao = "Peso normal";
+    else if (imc < 29,9) classificacao = "Sobrepeso";
     else classificacao = "Obesidade";
 
     resultado.textContent = `IMC: ${imc} - ${classificacao}`;
